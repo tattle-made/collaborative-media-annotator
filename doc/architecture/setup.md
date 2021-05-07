631c98ae8b7aa76e680179b10104d2899ec71cd6
dennyabrain <denny.george90@gmail.com>
2021-05-07 23:24:43 +0530

    setup eslint and prettier

---
+++ doc/architecture/setup.md
@@ -0,0 +1,5 @@
+# Formatting and Linting
+
+Uses prettier for code formatting and eslint for flagging syntax and code style convetions
+the configurations are defined in .prettierrc.json and .eslintrc.json
+We use VS Code extensions to ensure these are run during development
--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,44 +1,44 @@
-import React from 'react'
-import {Grommet, Box} from 'grommet'
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
-import Assignments from './pages/Assignments'
-import Assignment from './pages/Assignment'
-import Home from './pages/Home'
+import React from "react";
+import { Grommet, Box } from "grommet";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Assignments from "./pages/Assignments";
+import Assignment from "./pages/Assignment";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Grommet full>
-        <Router>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">home</Link>
-              </li>
-              <li>
-                <Link to="/assignments">assignments</Link>
-              </li>
-              <li>
-                <Link to="/assignment">assignment</Link>
-              </li>
-            </ul>
-          </nav>
- 
-          <Box>
-            <Switch>
-              <Route path="/" exact>
-                <Home/>
-              </Route>
-              <Route path="/assignments">
-                <Assignments/>
-              </Route>
-              <Route path="/assignment">
-                <Assignment/>
-              </Route>
-            </Switch>
-          </Box>
-                 </Router>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">home</Link>
+            </li>
+            <li>
+              <Link to="/assignments">assignments</Link>
+            </li>
+            <li>
+              <Link to="/assignment">assignment</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Box>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/assignments">
+              <Assignments />
+            </Route>
+            <Route path="/assignment">
+              <Assignment />
+            </Route>
+          </Switch>
+        </Box>
+      </Router>
     </Grommet>
-  )
+  );
 }
 
-export default App
+export default App;
